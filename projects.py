da0c882da9c1b59735d25e3a89067c2f8b166897
Brian Jensen <brian.jensen.example@gmail.com>
2024-01-08 10:46:37 -0700

    Add projects list and templates

---
+++ projects.py
@@ -0,0 +1,38 @@
+projects = [
+    {
+        "image": "/static/images/gtn.png",
+        "name": "Number Guessing Game",
+        "techs": ["Functions", "Loops", "Conditionals", "Lists"],
+        "description": """
+          A console number guessing game that prompts a player to choose a 
+          number between a specified range of numbers. After the user guesses 
+          the correct number, display the number of attempts it took them to 
+          guess correctly and the mean, median, and mode of the high scores.
+        """,
+        "live": "#",
+        "repo": "https://github.com/brian-example/a-number-guessing-game"
+    },
+    {
+        "image": "/static/images/bst.png",
+        "name": "Basketball Stats Tool",
+        "techs": ["Dictionaries", "Mean", "Median", "Mode"],
+        "description": """
+          A console basketball team statistics tool to help you divide up a 
+          group of players into teams and then provide a team analysis to 
+          ensure teams are evenly matched.
+        """,
+        "live": "#",
+        "repo": "https://github.com/brian-example/a-basketball-stats-tool"
+    },
+    {
+        "image": "/static/images/ph.png",
+        "name": "Phrase Hunters",
+        "techs": ["Object Oriented Programming", "Classes", "Methods"],
+        "description": """
+          A console game where players have to guess a random phrase letter
+          by letter before they run out of attempts.
+        """,
+        "live": "#",
+        "repo": "https://github.com/brian-example/phrase-hunters"
+    },
+]
